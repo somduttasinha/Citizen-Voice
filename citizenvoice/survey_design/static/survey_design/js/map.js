d96e488377cb5afcc1fe31a0e12c791ed4c2bd0d
JIJpma <d.j.ijpma@gmail.com>
2022-09-01 13:54:31 +0200

    Add user registration

--- citizenvoice/survey_design/static/survey_design/js/map.js
+++ citizenvoice/survey_design/static/survey_design/js/map.js
@@ -5,46 +5,37 @@ const map = L.map('map')
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
 map.setView(longLat, 12);
 
-// Add Marker
 
-var circle = L.circle(longLat, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.3,
-    radius: 4000
-})
-//.addTo(map);
-var group = L.featureGroup();
-//circle.addTo(group)
-map.addLayer(group)
 
-// Popup
+var group_circle = L.featureGroup();
+map.addLayer(group_circle)
 
-var group_green_circle = L.featureGroup();
-map.addLayer(group_green_circle)
-
-function onMapLeftClick(e) {
-//    popup
-//        .setLatLng(e.latlng)
-//        .setContent("You clicked the map at " + e.latlng.toString())
-//        .openOn(map);
-    other_circle = L.circle(e.latlng, {
+function onMapLeftClick_green(e) {
+    L.circle(e.latlng, {
         color: 'green',
         fillColor: '#f03',
         fillOpacity: 0.3,
         radius: 2000
-    }).addTo(group_green_circle)
+    }).addTo(group_circle)
 }
 
-var popup = L.popup();
-function onMapRightClick(e) {
-    alert("some text")
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+function onMapLeftClick_red(e) {
+    L.circle(e.latlng, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.3,
+        radius: 2000
+    }).addTo(group_circle)
 }
 
-map.on('click', onMapLeftClick);
-map.on('contextmenu', onMaprightClick);
+//map.on('click', onMapLeftClick_green);
 
+function select_circle_green() {
+    alert("You clicked Green");
+    map.on('click', onMapLeftClick_green);
+}
+
+function select_circle_red() {
+    alert("You clicked Red");
+    map.on('click', onMapLeftClick_red);
+}
