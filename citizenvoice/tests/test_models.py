1d65e15232f0e8b656f04c32c7abe6586d522bd9
Gerdus van der Laars <gerdus13@gmail.com>
2022-09-02 14:29:49 +0200

    Updated testing code for models.

--- citizenvoice/tests/test_models.py
+++ citizenvoice/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from .models import Question, Survey, Answer, Response
+from apiapp.models import Question, Survey, Answer, Response
 from django.contrib.auth.models import User
+from datetime import date
 
 
 class ModelTest(TestCase):
@@ -8,7 +9,11 @@ class ModelTest(TestCase):
     def setUpTestData(cls):
         print("setUpTestData: Run once to set up non-modified data for all class methods.")
         user = User(username='testuser', password='testpass')
-        survey = Survey(name='Test Survey 1', description='This is used to test things')
+        user.save()
+        survey = Survey(name='Test Survey 1', description='This is used to test things',
+                        display_method=1, template='abcd', publish_date=date.today(),
+                        expire_date=date.today(), redirect_url='www.google.com', author=user)
+        survey.save()
         print(survey)
         Question.objects.create(text='Testing question', order=1, required=True,
                                 question_type='text', choices='', survey=survey) 
@@ -17,7 +22,7 @@ class ModelTest(TestCase):
     def test_created_label(self):
         question = Question.objects.get(id=1)
         field_label = question._meta.get_field('text').verbose_name
-        self.assertEqual(field_label, 'text')
+        self.assertEqual(field_label, 'Text of the Question')
 
     def test_question_type_max_length(self):
         question = Question.objects.get(id=1)
