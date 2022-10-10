b77cb6827f114a01a3cdb42da4469f9860c1913b
cyasl <yaren.aslan@metu.edu.tr>
2022-10-10 14:57:47 +0200

    Create Response and Answer objects using forms

--- citizenvoice/respondent/urls.py
+++ citizenvoice/respondent/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='respondent-index'),
     path('survey', views.survey, name='respondent-home'),
     path('survey/<int:survey_id>/', views.survey_detail, name='respondent-survey-detail'),
-    path('survey/<int:survey_id>/question/<int:question_order>/', views.question_detail, name='respondent-question-detail'),
+    path('survey/<int:survey_id>/question/<int:question_order>/response/<int:response_id>/', views.question_detail,
+         name='respondent-question-detail'),
 ]
 
--- citizenvoice/respondent/views.py
+++ citizenvoice/respondent/views.py
@@ -1,18 +1,21 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from apiapp.views import SurveyViewSet, QuestionViewSet
-
+from apiapp.views import SurveyViewSet, QuestionViewSet, ResponseViewSet
+from .forms import ResponseCreationForm, AnswerCreationForm
+from django.utils import timezone
+import pytz
 
 # Create your views here.
 
+
 def index(request):
-    form = UserCreationForm()
     context = {
         'surveys': SurveyViewSet.GetSurveyByDesigner(request.user.id)
     }
     #TODO: Get all available surveys, do not filter by designer
     return render(request, 'respondent/index.html', context)
+
 
 def survey(request):
     context = {
@@ -21,9 +24,24 @@ def survey(request):
     }
     return render(request, 'respondent/index.html', context)
 
-
 def survey_detail(request, survey_id):
+    form = ResponseCreationForm()
+
+    if request.method == 'POST':
+        form = ResponseCreationForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.created = timezone.now()
+            obj.updated = timezone.now()
+            obj.survey = SurveyViewSet.GetSurveyByID(survey_id)[0]
+            #TODO: Discuss whether following fields are optional or not
+            obj.interview_uuid = "123"
+            obj.respondent = request.user
+            obj.save()
+        return redirect(question_detail, survey_id, 1, obj.id)
+
     context = {
+        'form': form,
         'survey_id': survey_id
     }
     try:
@@ -33,27 +51,45 @@ def survey_detail(request, survey_id):
         # pass for now, we might add some warning in the future
         raise e
     return render(request, 'respondent/index.html', context)
-    # return render(request, '../../survey_design/templates/survey_design/survey.html', context)
 
-@login_required
-def question_detail(request, survey_id, question_order):
-    # TODO Check whether survey_id is owned by currently logged in user
+
+def question_detail(request, survey_id, question_order, response_id):
+    form = AnswerCreationForm()
+
+    if request.method == 'POST':
+        form = AnswerCreationForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.created = timezone.now()
+            obj.updated = timezone.now()
+            obj.response = ResponseViewSet.GetResponseByID(response_id)[0]
+            obj.question = QuestionViewSet.GetOrderedQuestionBySurvey(survey_id, question_order)[0]
+            obj.save()
+        if SurveyViewSet.GetSurveyByID(survey_id)[0].question_count() > question_order:
+            return redirect(question_detail, survey_id, question_order + 1, response_id)
+        else:
+            return redirect(index)
+
     context = {
+        'form': form,
         'title': 'Survey Design',
         'surveys': SurveyViewSet.GetSurveyByDesigner(request.user.id),
         'survey_id': survey_id,
         'respondent': True,
+        'button_value': "End Survey"
     }
     try:
-        context['survey_to_display'] = SurveyViewSet.GetSurveyByID(survey_id)[0]
-        context['question_to_display'] = QuestionViewSet.GetOrderedQuestionBySurvey(survey_id, question_order)[0]
+        context['survey'] = SurveyViewSet.GetSurveyByID(survey_id)[0]
+        context['question'] = QuestionViewSet.GetOrderedQuestionBySurvey(survey_id, question_order)[0]
 
-        print(context['survey_to_display'].question_count(), question_order)
-        if context['survey_to_display'].question_count() > question_order:
+        if context['survey'].question_count() > question_order:
             context['next_question_order'] = question_order + 1
+            context['button_value'] = "Next Question"
 
     except Exception as e:
         # Survey.DoesNotExist
         # pass for now, we might add some warning in the future
         raise e
-    return render(request, '../../survey_design/templates/survey_design/survey.html', context)
+
+    return render(request, 'respondent/index.html', context)
+
