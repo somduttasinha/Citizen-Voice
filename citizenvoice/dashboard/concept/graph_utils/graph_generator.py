f5dd300edb227f4c8c4e43b2446d91f0dbd6f326
Somdutta Sinha <somdsinha@gmail.com>
2022-10-17 12:14:56 +0200

    implemented create_choropleth method

--- citizenvoice/dashboard/concept/graph_utils/graph_generator.py
+++ citizenvoice/dashboard/concept/graph_utils/graph_generator.py
@@ -7,18 +7,18 @@ class Generator:
     def __init__(self, df: pd.DataFrame):
         self.dataframe = df
 
-    def create_choropleth(self, geojson_file):  # eventually use kwargs to configure graph settings
-        '''
-        Uses geojson file
-        '''
+    def create_choropleth(self, geojson_file, column_name):  # eventually use kwargs to configure graph settings
+        """
+        Uses geojson file provided to create a choropleth map using the statistic given by column_name
+        """
 
         parser = Parser(self.dataframe)
         df = parser.combine_df_with_geojson(geojson_file)
-
-        fig = px.choropleth_mapbox(population_2011,
+        map_df = df[["Area Name", column_name, "id"]]
+        fig = px.choropleth_mapbox(map_df,
                                    locations="id",
                                    geojson=geojson_file,
-                                   color="Persons-2011",
+                                   color=column_name,
                                    hover_name="Area Name",
                                    mapbox_style="carto-positron",
                                    center={
