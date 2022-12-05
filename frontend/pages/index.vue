e16520e5f9c88cd7d81110d2bdc5c107eb079826
JIJpma <d.j.ijpma@gmail.com>
2022-12-05 10:15:43 +0100

    add Vue Login and WIP Survey Design

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-    <NuxtLayout name="default">
-
-    </NuxtLayout>
+  <NuxtLayout name="default">
+    <q-page>
+      <h1>Home</h1>
+    </q-page>
+  </NuxtLayout>
 </template>
 
 <script setup>
 
 </script>
+
+<style lang="scss" scoped>
+.custom-bg-white {
+  background-color: #fff;
+}
+</style>
