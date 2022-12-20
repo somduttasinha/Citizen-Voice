df2735d796abe087de84efc5a43ca2d46c526f39
Som Sinha <somdsinha@gmail.com>
2022-12-20 12:08:14 +0100

    create Banner component and include in index.vue

---
+++ frontend/components/Banner.vue
@@ -0,0 +1,22 @@
+<template>
+    <q-banner class="bg-primary text-white">
+      <h2>{{ mainText }}</h2>
+      <h3>{{ supportingText }}</h3>
+    </q-banner>
+
+</template>
+  
+  <script>
+  import { matAllInbox } from '@quasar/extras/material-icons'
+  export default {
+    name: "Banner",
+    props: {
+      mainText : String,
+      supportingText : String
+    }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <NuxtLayout name="default">
     <q-page>
-      <h1>Home</h1>
+      <h1>Citizen Voice</h1>
+      <Banner mainText = "Welcome to Citizen Voice!" supportingText = "This is a channel to voice your experiences, needs and aspirations."/>
     </q-page>
   </NuxtLayout>
 </template>
 
 <script setup>
+import Banner from "../components/Banner"
 
 </script>
 
