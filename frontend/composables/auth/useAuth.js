5a3a2be24282fb3bc0b0909fd809917ad84d4e2f
esthetech <esthetech@riseup.net>
2022-12-21 10:28:11 +0100

    wip

--- frontend/composables/auth/useAuth.js
+++ frontend/composables/auth/useAuth.js
@@ -21,12 +21,18 @@ export const useAuth = () => {
         email,
         password,
     ) => {
+        // What we need:
+        // Create cookies with the HttpOnly attribute and the `Secure` attribute
+        // `HttpOnly` is the one which controls whether the cookie is made available to JavaScript.
+        // The `Secure` attribute is for guaranteeing the cookie is only served over HTTPS connections, avoiding MITM attacks.
+        // Also the `SameSite` attribute controls whether 3rd party sites have access to it so it is still helpful against a CSRF attack leaking the cookie.
         const data = await $fetch('/api/auth/login/', {
             method: 'POST',
             body: {
                 email,
                 password,
             },
+            withCredentials: true
         })
 
         console.log('data //> ', data, data.user)
