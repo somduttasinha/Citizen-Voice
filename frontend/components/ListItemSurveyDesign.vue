10df22a56af4f477f55c60bdbee7bff33d6683f5
Gerdusvdl <52563982+Gerdusvdl@users.noreply.github.com>
2023-01-03 13:32:17 +0100

    Included survey delete button functionality

--- frontend/components/ListItemSurveyDesign.vue
+++ frontend/components/ListItemSurveyDesign.vue
@@ -1,11 +1,15 @@
+<style scoped>
+
+</style>
+
 <template>
   <q-item clickable>
     <q-item-section avatar top>
       <q-icon name="description" color="black" size="30px" />
     </q-item-section>
 
-    <q-item-section top class="col-2 gt-sm">
-      <q-item-label class="q-mt-sm">{{survey_object.name}}</q-item-label>
+    <q-item-section top class="col-2">
+      <q-item-label class="q-mt-sm">{{ props.survey_object.name }}</q-item-label>
     </q-item-section>
 
     <q-item-section top>
@@ -25,21 +29,50 @@
       <div class="text-grey-8 q-gutter-xs">
         <q-btn class="gt-xs" size="12px" flat dense round icon="done" />
         <q-btn size="12px" flat dense round icon="more_vert" />
-        <q-btn class="gt-xs" size="12px" flat dense round icon="delete" />
+        <q-btn class="gt-xs" size="12px" flat dense round icon="delete" @click="deleteHandler" />
       </div>
     </q-item-section>
   </q-item>
 </template>
 
-<script>
+<script setup>
 import { matAllInbox } from '@quasar/extras/material-icons'
-export default {
-  name: "ListItemSurveyDesign",
-  props: {
-    survey_object: Object
-  }
+const props = defineProps({
+  survey_object: Object
+})
+
+const delete_url = "/api/surveys/";
+
+const deleteHandler = () => {
+  console.log('click');
+  // DELETE request using fetch with error handling
+  const survey_object = props.survey_object;
+  console.log(survey_object.id);
+  const requestOptions = {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  };
+  fetch(delete_url + survey_object.id, requestOptions)
+    .then(async response => {
+      const data = await response.json();
+
+      // check for error response
+      if (!response.ok) {
+        // get error message from body or default to response status
+        const error = (data && data.message) || response.status;
+        return Promise.reject(error);
+      }
+
+      this.postId = data.id;
+    })
+    .catch(error => {
+      errorMessage = error;
+      console.error('There was an error!', error);
+    });
 }
+
 </script>
 
 <style scoped>
+
 </style>
