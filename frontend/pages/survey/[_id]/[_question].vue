d6b9dcf048fbde26aaa884f60bfc19b1aae043a9
cyasl <yaren.aslan@metu.edu.tr>
2023-01-09 14:01:13 +0100

    Implement map and circles

--- frontend/pages/survey/[_id]/[_question].vue
+++ frontend/pages/survey/[_id]/[_question].vue
@@ -1,7 +1,6 @@
 <template>
     <NuxtLayout name="default">
 
-
         <div class="q-pa-md row items-start q-gutter-md">
           <!-- Question card: number & text -->
           <q-card class="my-card">
@@ -13,20 +12,31 @@
         </div>
 
         <div class="q-pa-md row items-start q-gutter-md">
-          <!-- Map card
-          real v-if statement = (question.map_view != null || question.is_geospatial)-->
-          <q-card v-if="question.is_geospatial"
-                  style="min-width: 300px;" class="my-card col">
+          <!-- Map card -->
+          <q-card v-show="(question.map_view != null || question.is_geospatial)"
+                  style="min-width: 600px;" class="my-card col">
             <q-card-section>
-              <div class="text-h5 q-mt-sm q-mb-xs">Map here</div>
-              <div id="map"></div>
+                <div style="height:300px; width:600px">
+                  <l-map ref="map" v-model:zoom="zoom" :center="center" :minZoom="1" :maxZoom="18">
+                    <l-tile-layer
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      layer-type="base"
+                      name="OpenStreetMap"
+                    ></l-tile-layer>
+                    <l-circle v-for="circle, index in circles""
+                              :lat-lng="circle"
+                              :radius="circleSettings.radius"
+                              :color="circleSettings.circleColor"
+                    ></l-circle>
+                  </l-map>
+                </div>
             </q-card-section>
           </q-card>
 
           <!-- Answer card-->
           <q-card style="min-width: 300px;" class="my-card col">
             <q-card-section>
-
+              <p> Answer here </p>
             </q-card-section>
           </q-card>
         </div>
@@ -50,9 +60,12 @@
 
 
 <script setup>
+
   import { ref } from "vue"
   import {navigateTo} from "nuxt/app";
-  import leaflet from "leaflet"
+  import "leaflet/dist/leaflet.css";
+  import { LMap, LTileLayer, LCircle} from "@vue-leaflet/vue-leaflet";
+
   /**
    * All `/api/**` are proxies pointing to the local or production server of the backend.
    */
@@ -60,14 +73,26 @@
   const question_url = "/api/questions/"
   const data = ref([])
   const route = useRoute()
-
   const { data: survey } = await useAsyncData(() => $fetch(survey_url + route.params._id));
-
   // TODO: use an API to get n'th question of the selected survey
   // for demo only, I will use (5 + question id).
   let demo_question = parseInt(route.params._question, 10) + 5
   let { data: question } = await useAsyncData(() => $fetch(question_url + demo_question));
 
+  // to set up the map
+  // these values will be replaced as soon as API for map_view is configured
+  const zoom = ref(4)
+  const center = ref([47.41322, -1.219482])
+  const circleSettings = ref(
+    {circleColor: 'red', radius: 3000}
+  )
+  const circles = ref([
+        L.latLng(47.412, -1.218),
+        L.latLng(47.413220, -1.219482),
+        L.latLng(47.414, -1.22),
+      ])
+
+  // to navigate from one question to the previous/next
   const prevQuestion = async () => {
     // if this is not the first question:
     let question_to_navigate = (parseInt(route.params._question, 10) - 1)
@@ -77,7 +102,6 @@
       return navigateTo('/survey/' + route.params._id)
     }
   }
-
   const nextQuestion = async () => {
     // if this is not the last question:
     return navigateTo('/survey/' + route.params._id + '/' + (parseInt(route.params._question, 10) + 1))
