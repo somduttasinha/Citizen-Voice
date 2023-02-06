43013d6f19f105023c31c07207fe64a31c94bb8f
esthetech <esthetech@riseup.net>
2023-02-06 19:12:11 +0100

    adds survey design pages create and single detail page

--- frontend/components/ListItemSurveyDesign.vue
+++ frontend/components/ListItemSurveyDesign.vue
@@ -3,36 +3,36 @@
 </style>
 
 <template>
-  <q-item clickable>
-    <q-item-section avatar top>
-      <q-icon name="description" color="black" size="30px" />
-    </q-item-section>
+    <q-item clickable :to="`/design/surveys/${props.survey_object.id}`">
+        <q-item-section avatar top>
+            <q-icon name="description" color="black" size="30px" />
+        </q-item-section>
 
-    <q-item-section top class="col-2">
-      <q-item-label class="q-mt-sm">{{ props.survey_object.name }}</q-item-label>
-    </q-item-section>
+        <q-item-section top class="col-2">
+            <q-item-label class="q-mt-sm">{{ props.survey_object.name }}</q-item-label>
+        </q-item-section>
 
-    <q-item-section top>
-      <!--        <q-item-label lines="1">-->
-      <!--          <span class="text-weight-medium">[quasarframework/quasar]</span>-->
-      <!--          <span class="text-grey-8"> - GitHub repository</span>-->
-      <!--        </q-item-label>-->
-      <!--        <q-item-label caption lines="1">-->
-      <!--          @rstoenescu in #3: > Generic type parameter for props-->
-      <!--        </q-item-label>-->
-      <!--        <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase">-->
-      <!--          <span class="cursor-pointer">Open in GitHub</span>-->
-      <!--        </q-item-label>-->
-    </q-item-section>
+        <q-item-section top>
+            <!--        <q-item-label lines="1">-->
+            <!--          <span class="text-weight-medium">[quasarframework/quasar]</span>-->
+            <!--          <span class="text-grey-8"> - GitHub repository</span>-->
+            <!--        </q-item-label>-->
+            <!--        <q-item-label caption lines="1">-->
+            <!--          @rstoenescu in #3: > Generic type parameter for props-->
+            <!--        </q-item-label>-->
+            <!--        <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase">-->
+            <!--          <span class="cursor-pointer">Open in GitHub</span>-->
+            <!--        </q-item-label>-->
+        </q-item-section>
 
-    <q-item-section top side>
-      <div class="text-grey-8 q-gutter-xs">
-        <q-btn class="gt-xs" size="12px" flat dense round icon="done" />
-        <q-btn size="12px" flat dense round icon="more_vert" />
-        <q-btn class="gt-xs" size="12px" flat dense round icon="delete" @click="deleteHandler" />
-      </div>
-    </q-item-section>
-  </q-item>
+        <q-item-section top side>
+            <div class="text-grey-8 q-gutter-xs">
+                <q-btn class="gt-xs" size="12px" flat dense round icon="done" />
+                <q-btn size="12px" flat dense round icon="more_vert" />
+                <q-btn class="gt-xs" size="12px" flat dense round icon="delete" @click="deleteHandler" />
+            </div>
+        </q-item-section>
+    </q-item>
 </template>
 
 <script setup>
@@ -40,16 +40,16 @@ import { matAllInbox } from '@quasar/extras/material-icons'
 import { useSurveyStore } from "~/stores/survey"
 
 const props = defineProps({
-  survey_object: Object
+    survey_object: Object
 })
 
 
 const surveyStore = useSurveyStore()
 
 const deleteHandler = async () => {
-  // DELETE request using the surveyStore with error handling
-  const survey_object = props.survey_object;
-  await surveyStore.deleteSurvey(survey_object.id)
+    // DELETE request using the surveyStore with error handling
+    const survey_object = props.survey_object;
+    await surveyStore.deleteSurvey(survey_object.id)
 }
 
 </script>
---
+++ frontend/pages/design/index.vue
@@ -0,0 +1,109 @@
+<template>
+    <NuxtLayout name="default">
+        <q-page>
+            <div class="padding-16">
+
+                <div class="container">
+                    <div class="">
+                        <h2 class="inline-block">My Surveys test</h2>
+                        <q-btn class="button" color="white" text-color="black" label="Add survey"
+                            to="/design/surveys/create" />
+                    </div>
+                    <q-list bordered class="rounded-borders custom-width-60-pc" style="max-width: 800px">
+                        <!--          <q-item-label header>Google Inbox style</q-item-label>-->
+                        <list-item-survey-design v-for="survey in surveys" :survey_object="survey">
+                        </list-item-survey-design>
+                        <!--          <q-separator/>-->
+                    </q-list>
+                    <!-- <q-input v-model="textName" label="Name" />
+                    <q-input v-model="textDescription" label="Description" />
+                    <q-btn class="h-min" color="white" text-color="black" label="Add survey" @click="addNewSurvey" /> -->
+
+                </div>
+            </div>
+        </q-page>
+    </NuxtLayout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import BaseButton from "~/components/BaseButton";
+import ListItemSurveyDesign from "~/components/ListItemSurveyDesign";
+import { formatDate } from "~/utils/formatData"
+import { useSurveyStore } from "~/stores/survey"
+
+// Make sure the user is authenticated or trigger the reroute to login
+definePageMeta({ middleware: 'authorization' })
+
+/**
+ * All `/api/**` are proxies pointing to the local or production server of the backend.
+ */
+
+const url = "/api/surveys/"
+const surveyStore = useSurveyStore()
+const { data: surveys } = await useAsyncData(() => $cmsApi(url));
+var expire_date = new Date();
+var current_date = new Date();
+const textName = ref(null)
+const textDescription = ref(null)
+
+// set default expire date 100 days after current day
+expire_date.setDate(expire_date.getDate() + 100);
+current_date.setDate(current_date.getDate());
+
+// Add a new survey using the surveyStore, based on what is entered in the field.
+const addNewSurvey = async () => {
+    await surveyStore.createSurvey(textName.value, textDescription.value, current_date, expire_date)
+}
+
+</script>
+
+<style lang="scss" scoped>
+.custom-bg-white {
+    background-color: #fff;
+}
+
+.custom-list-item {
+    //min-height: 50vh;
+    height: calc(100vh/3);
+    background: #fff;
+    overflow: hidden;
+    overflow-y: scroll;
+}
+
+::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+    /* make scrollbar transparent */
+}
+
+.custom-scroll {
+    position: relative;
+    max-height: 100%;
+    overflow-y: scroll;
+}
+
+::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+    /* make scrollbar transparent */
+}
+
+.custom-bg-red {
+    min-width: 100%;
+    min-height: 100%;
+    background: #b02a37;
+}
+
+.custom-width-60-pc {
+    width: 60%;
+}
+
+.padding-16 {
+    padding: 16px;
+}
+
+.button {
+    margin-left: 24px;
+}
+</style>
