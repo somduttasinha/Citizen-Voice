d7c3f450963518224b92d90b879c51df9e95df38
esthetech <esthetech@riseup.net>
2023-02-07 14:46:16 +0100

    Adds header and Vuetify layouts

---
+++ frontend/components/Header.vue
@@ -0,0 +1,111 @@
+<template>
+    <v-card>
+        <v-app-bar color="primary">
+            <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+            <v-app-bar-title><img class="full-height q-py-sm" height="30" width="100"
+                    src="~/assets/icons/civo-logo.svg" /></v-app-bar-title>
+
+            <v-spacer></v-spacer>
+
+            <!-- <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn> -->
+
+            <v-menu open-on-hover>
+                <template v-slot:activator="{ props }">
+                    <v-btn class="text-none" color="white" v-bind="props">
+                        <v-icon size="x-large">mdi-account</v-icon>
+                    </v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item v-if="isAuthenticated" to="/user">
+                        <v-list-item-title>Profile</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item v-if="!isAuthenticated" to="/login">
+                        <v-list-item-title>Login</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item v-if="!isAuthenticated" to="/register">
+                        <v-list-item-title>Register</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item v-if="isAuthenticated" @click="logoutHandler($q)">
+                        <v-list-item-title>Logout</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+
+            <template v-slot:extension>
+                <v-tabs v-model="tab" align-tabs="title">
+                    <v-tab v-for="item in items" :key="item.label" :value="item.value" :to="item.link">
+                        {{ item.label }}
+                    </v-tab>
+                </v-tabs>
+            </template>
+        </v-app-bar>
+    </v-card>
+
+</template>
+
+<script>
+import { useUserStore } from "~/stores/user"
+
+export default {
+    data() {
+        return {
+            tab: null,
+            items: [
+                {
+                    label: 'Home',
+                    link: '/'
+                },
+                {
+                    label: 'Surveys',
+                    link: '/surveys'
+                },
+                {
+                    label: 'Design survey',
+                    link: '/design'
+                },
+                {
+                    label: 'About',
+                    link: '/about'
+                }
+            ],
+            itemsAccount: [
+                {
+                    label: 'Profile',
+                    link: '/user'
+                },
+                {
+                    label: 'Login',
+                    link: '/login'
+                },
+                {
+                    label: 'Profile',
+                    link: '/user'
+                },
+                {
+                    label: 'Profile',
+                    link: '/user'
+                }
+            ]
+        }
+    },
+    setup() {
+        const userStore = useUserStore()
+
+        const logoutHandler = async ($q) => {
+            await userStore.logout($q)
+            if (!userStore.isAuthenticated) {
+                await navigateTo('/')
+            }
+        }
+
+        return {
+            isAuthenticated: userStore.isAuthenticated,
+            logoutHandler,
+        }
+    }
+}
+</script>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,98 +1,23 @@
 <template>
-    <!-- ClientOnly is needed for quasar to work -->
-    <ClientOnly>
-        <q-layout view="hHh lpR fFf">
-
-            <q-header elevated class="bg-primary text-white" height-hint="98">
-                <q-toolbar>
-                    <q-toolbar-title>
-                        <img class="full-height q-py-sm" height="30" width="100" src="~/assets/icons/civo-logo.svg">
-                    </q-toolbar-title>
-                </q-toolbar>
-
-                <div class="row justify-between">
-                    <q-tabs align="left">
-                        <q-route-tab to="/" label="Home" />
-                        <q-route-tab to="/design" label="Start designing" />
-                        <q-route-tab to="/surveys" label="Start answering" />
-                        <q-route-tab to="/about" label="About" />
-                    </q-tabs>
-
-                    <q-tabs align="right">
-                        <q-btn-dropdown auto-close flat round stretch icon="person" size="18px">
-                            <q-list>
-                                <q-item v-if="isAuthenticated" tag="a" to="/user">
-                                    <q-item-section>Profile</q-item-section>
-                                </q-item>
-                                <q-item v-if="!isAuthenticated" tag="a" to="/login">
-                                    <q-item-section>Login</q-item-section>
-                                </q-item>
-                                <q-item v-if="!isAuthenticated" tag="a" to="/register">
-                                    <q-item-section>Register</q-item-section>
-                                </q-item>
-                                <q-item v-if="isAuthenticated" clickable @click="logoutHandler($q)">
-                                    <q-item-section>Logout</q-item-section>
-                                </q-item>
-                            </q-list>
-                        </q-btn-dropdown>
-
-                        <!-- <q-route-tab to="/login" label="Login" /> |
-                        <q-route-tab to="/register" label="Register" /> -->
-                    </q-tabs>
-                </div>
-            </q-header>
-
-
-            <q-page-container>
-                <!--                <q-page padding>-->
+    <v-app>
+        <Header />
+        <v-main>
+            <v-container fluid>
                 <slot />
-                <!--                </q-page>-->
-            </q-page-container>
-
-            <q-footer elevated class="bg-grey-8 text-white">
-                <q-toolbar>
-                    <q-toolbar-title>
-                        <img class="full-height q-py-sm" height="30" width="100" src="~/assets/icons/civo-logo.svg">
-                    </q-toolbar-title>
-                </q-toolbar>
-            </q-footer>
-
-        </q-layout>
-    </ClientOnly>
+            </v-container>
+        </v-main>
+        <v-footer>
+            <img class="full-height q-py-sm" height="30" width="100" src="~/assets/icons/civo-logo.svg" />
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { useUserStore } from "~/stores/user"
-
 export default {
-    setup() {
-        const userStore = useUserStore()
-        const leftDrawerOpen = ref(false)
-        const rightDrawerOpen = ref(false)
-
-
-        const logoutHandler = async ($q) => {
-            await userStore.logout($q)
-            if (!userStore.isAuthenticated) {
-                await navigateTo('/')
-            }
-        }
-
-        return {
-            isAuthenticated: userStore.isAuthenticated,
-            logoutHandler,
-            leftDrawerOpen,
-            toggleLeftDrawer() {
-                leftDrawerOpen.value = !leftDrawerOpen.value
-            },
-            rightDrawerOpen,
-            toggleRightDrawer() {
-                rightDrawerOpen.value = !rightDrawerOpen.value
-            }
-
-        }
-    }
+    data() {
+        return {}
+    },
+    setup() { }
 }
 </script>
 
@@ -102,3 +27,59 @@ export default {
     padding-left: 0;
 }
 </style>
+<!-- 
+<q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+        <q-toolbar>
+            <q-toolbar-title>
+                <img class="full-height q-py-sm" height="30" width="100" src="~/assets/icons/civo-logo.svg">
+            </q-toolbar-title>
+        </q-toolbar>
+
+        <div class="row justify-between">
+            <q-tabs align="left">
+                <q-route-tab to="/" label="Home" />
+                <q-route-tab to="/design" label="Start designing" />
+                <q-route-tab to="/surveys" label="Start answering" />
+                <q-route-tab to="/about" label="About" />
+            </q-tabs>
+
+            <q-tabs align="right">
+                <q-btn-dropdown auto-close flat round stretch icon="person" size="18px">
+                    <q-list>
+                        <q-item v-if="isAuthenticated" tag="a" to="/user">
+                            <q-item-section>Profile</q-item-section>
+                        </q-item>
+                        <q-item v-if="!isAuthenticated" tag="a" to="/login">
+                            <q-item-section>Login</q-item-section>
+                        </q-item>
+                        <q-item v-if="!isAuthenticated" tag="a" to="/register">
+                            <q-item-section>Register</q-item-section>
+                        </q-item>
+                        <q-item v-if="isAuthenticated" clickable @click="logoutHandler($q)">
+                            <q-item-section>Logout</q-item-section>
+                        </q-item>
+                    </q-list>
+                </q-btn-dropdown>
+
+                <q-route-tab to="/login" label="Login" /> |
+                <q-route-tab to="/register" label="Register" />
+            </q-tabs>
+        </div>
+    </q-header>
+
+
+<q-page-container>
+           <q-page padding>
+        <slot />
+ </q-page>
+    </q-page-container>
+
+<q-footer elevated class="bg-grey-8 text-white">
+        <q-toolbar>
+            <q-toolbar-title>
+                <img class="full-height q-py-sm" height="30" width="100" src="~/assets/icons/civo-logo.svg">
+            </q-toolbar-title>
+        </q-toolbar>
+    </q-footer> -->
