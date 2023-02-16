36aa1047bdc5d8b27b199a10598df4e615fa38f7
esthetech <esthetech@riseup.net>
2023-02-16 15:30:52 +0100

    Rewriting layout to use vuetify

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -12,13 +12,9 @@
     </v-app>
 </template>
 
-<script>
-export default {
-    data() {
-        return {}
-    },
-    setup() { }
-}
+<script setup>
+import Header from '@/components/Header.vue'
+
 </script>
 
 <style lang="scss" scoped>
@@ -27,59 +23,3 @@ export default {
     padding-left: 0;
 }
 </style>
-<!-- 
-<q-layout view="hHh lpR fFf">
-
-    <q-header elevated class="bg-primary text-white" height-hint="98">
-        <q-toolbar>
-            <q-toolbar-title>
-                <img class="full-height q-py-sm" height="30" width="100" src="~/assets/icons/civo-logo.svg">
-            </q-toolbar-title>
-        </q-toolbar>
-
-        <div class="row justify-between">
-            <q-tabs align="left">
-                <q-route-tab to="/" label="Home" />
-                <q-route-tab to="/design" label="Start designing" />
-                <q-route-tab to="/surveys" label="Start answering" />
-                <q-route-tab to="/about" label="About" />
-            </q-tabs>
-
-            <q-tabs align="right">
-                <q-btn-dropdown auto-close flat round stretch icon="person" size="18px">
-                    <q-list>
-                        <q-item v-if="isAuthenticated" tag="a" to="/user">
-                            <q-item-section>Profile</q-item-section>
-                        </q-item>
-                        <q-item v-if="!isAuthenticated" tag="a" to="/login">
-                            <q-item-section>Login</q-item-section>
-                        </q-item>
-                        <q-item v-if="!isAuthenticated" tag="a" to="/register">
-                            <q-item-section>Register</q-item-section>
-                        </q-item>
-                        <q-item v-if="isAuthenticated" clickable @click="logoutHandler($q)">
-                            <q-item-section>Logout</q-item-section>
-                        </q-item>
-                    </q-list>
-                </q-btn-dropdown>
-
-                <q-route-tab to="/login" label="Login" /> |
-                <q-route-tab to="/register" label="Register" />
-            </q-tabs>
-        </div>
-    </q-header>
-
-
-<q-page-container>
-           <q-page padding>
-        <slot />
- </q-page>
-    </q-page-container>
-
-<q-footer elevated class="bg-grey-8 text-white">
-        <q-toolbar>
-            <q-toolbar-title>
-                <img class="full-height q-py-sm" height="30" width="100" src="~/assets/icons/civo-logo.svg">
-            </q-toolbar-title>
-        </q-toolbar>
-    </q-footer> -->
