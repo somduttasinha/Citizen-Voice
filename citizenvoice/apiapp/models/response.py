365b7ee7efb45b4c749d2b88b64c8f0663a3af5d
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-11-03 16:55:26 +0100

    review code, add TODO's

--- citizenvoice/apiapp/models/response.py
+++ citizenvoice/apiapp/models/response.py
@@ -22,7 +22,7 @@ class Response(models.Model):
     updated = models.DateTimeField(_("Last edit"), auto_now=True)
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     interview_uuid = models.CharField(_("Unique ID of interview"), max_length=150)
-    respondent = models.ForeignKey(User, on_delete=models.CASCADE)
+    respondent = models.ForeignKey(User, on_delete=models.CASCADE) # TODO: [manuel] this should be optional as a user could be anonymous
 
     def __str__(self):
         return f"Response {self.pk} ({self.survey.name})"
