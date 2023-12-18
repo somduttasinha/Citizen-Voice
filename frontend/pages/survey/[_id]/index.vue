72f853e21837fd2b3373e99ba2f874cd12d26e9f
Manuel Garcia <gilbert.mg22@gmail.com>
2023-12-19 00:01:50 +0100

    Refactor survey page and survey store
    Add v-cards to display survey details and improve user experience
    
    Update createSurvey API call in survey store
    
    Comment out unused code in survey page
    
    Fix bug in createResponse function
    
    Improve code readability and remove console logs

--- frontend/pages/survey/[_id]/index.vue
+++ frontend/pages/survey/[_id]/index.vue
@@ -1,58 +1,79 @@
 <template>
     <NuxtLayout name="default">
         <div class="padding-16">
+          <v-sheet
+            class="d-flex align-center flex-column"
+            height="200"
+          >
+            <v-card-actions class="justify-center">
+              <h2>{{ survey.name }}</h2>
+            </v-card-actions>
+            <v-card-actions class="justify-center">
+              <p>{{ survey.description }} </p>
+            </v-card-actions>
+            <v-card-actions class="justify-center">
+              <p>Publish date: {{ formatDate(survey.publish_date) }}</p>
+            </v-card-actions>
+            <v-card-actions class="justify-center">
+              <p>Expire date: {{ formatDate(survey.expire_date) }}</p>
+            </v-card-actions>
+            <v-card-actions class="justify-center" >
+              <v-btn @click="createResponse" color="primary">
+                <i class="fa-solid fa-play"></i>
+                <span class="q-pa-sm">Start survey</span>
+              </v-btn>
+            </v-card-actions>
+          </v-sheet>
             <!-- <p>Counter: {{this.$store.state.counter}}</p>-->
-            <h2 v-if="survey">{{ survey.name }}</h2>
+            <h2>{{ survey.name }}</h2>
+<!--            <h2>{{ survey.name }}</h2>-->
 
-            <p v-if="survey">{{ survey.description }} </p>
-            <!-- <p v-if="survey">Publish date: {{ formatDate(survey.publish_date) }}</p> -->
-            <p v-if="survey">Expire date: {{ formatDate(survey.expire_date) }}</p>
+            <p>{{ survey.description }} </p>
+            <p>Publish date: {{ formatDate(survey.publish_date) }}</p>
+            <p>Expire date: {{ formatDate(survey.expire_date) }}</p>
+<!--            <p>{{ survey.description }} </p>-->
+<!--            <p>Publish date: {{ formatDate(survey.publish_date) }}</p>-->
+<!--            <p>Expire date: {{ formatDate(survey.expire_date) }}</p>-->
 
-            <v-btn @click="createResponse" color="secondary">
+            <v-btn @click="createResponse" color="primary">
                 <i class="fa-solid fa-play"></i>
                 <span class="q-pa-sm">Start survey</span>
             </v-btn>
+<!--            <v-btn @click="createResponse" color="primary">-->
+<!--                <i class="fa-solid fa-play"></i>-->
+<!--                <span class="q-pa-sm">Start survey</span>-->
+<!--            </v-btn>-->
 
             <!--{{ $route.params.id }}
                 <pre>{{ survey }}</pre> -->
         </div>
     </NuxtLayout>
 </template>
-
 <script setup>
 import { ref } from "vue"
 import { navigateTo } from "nuxt/app";
 import { useStoreResponse } from '~/stores/response'
-
 const storeResponse = useStoreResponse()
 const survey_url = "/api/surveys/"
 const create_response_url = "/api/responses/"
 const origin_url = "http://localhost:3000"
 const data = ref([])
 const route = useRoute()
-
-// retrieve survey from API
 const survey = await storeResponse.getSurvey({ id: route.params._id })
-
-// console.log('survey//>', survey)
-
+console.log('name of survey //', survey.value.name)
 const createResponse = async () => {
     // Make a POST request to your Django API endpoint to create a new Response object
-    const _response = await storeResponse.createResponse({surveyId: route.params._id})
-
-
-    if (_response) { // THIS ENSURES  respondentId is available
+    await storeResponse.createResponse({ id: route.params._id })
+    const respondentId = await storeResponse.createResponse({ id: route.params._id })
+    // Navigate to the /survey/${survey.id}/1 page after the response is created
+    if (respondentId) {
         // Navigate to the /survey/${survey.id}/1 page after the response is created
-
-        // Find how to retrieve and set the interviewerId to the stor
-        console.log('successfull creating response//>', _response)
         return navigateTo('/survey/' + route.params._id + '/1')
     }
-    else {
-        console.log('createResponse did not return anything//>')
-        // return navigateTo('/survey/' + route.params._id)
-    }
+
+    // if (respondentId) {
+    //     // Navigate to the /survey/${survey.id}/1 page after the response is created
+    //     return navigateTo('/survey/' + route.params._id + '/1')
+    // }
 }
-
 </script>
-
