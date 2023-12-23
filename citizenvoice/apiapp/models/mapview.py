9c1e1e5c9f82d692fd1bb79cb2e189bced374426
Manuel Garcia <gilbert.mg22@gmail.com>
2023-12-23 02:05:18 +0100

    Fix JSON validation comment in MapView model

--- citizenvoice/apiapp/models/mapview.py
+++ citizenvoice/apiapp/models/mapview.py
@@ -18,7 +18,7 @@ class MapView(models.Model):
     name = models.CharField(_("Name of the MapView location"), max_length=150, default="Delft")
     map_service_url = models.CharField(_("Map Service URL"), max_length=150,
                                        default=default_service_url)
-    # TODO: add JSON validation to see if the data that is being stored is validated JSON
+    # TODO: add JSON validation to see if the data that is being stored is valid JSON
     options = models.JSONField(_("Map service specific options"), default=default_options)
     geometries = models.JSONField(_("Custom geometries for the map view"), blank=True, null=True) # TODO: [manuel] this should be a GeoJSONField, can DJANGO SUPPORT THIS? if not, we should do our own validation
 
