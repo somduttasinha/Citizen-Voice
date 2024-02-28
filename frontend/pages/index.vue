063d401bb72b55ce89ea67629a64beb25b4fdf51
Manuel Garcia <gilbert.mg22@gmail.com>
2024-02-28 17:12:25 +0100

    Add content to home page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,23 @@
 <template>
     <NuxtLayout name="default">
         <h1 class="pt-10">Home</h1>
+
+         <v-card class="my-card" title="Welcome to the home page" subtitle="This is the home page">   
+
+        <div class="my-card col">
+            <p>
+            Ullamco eu quis mollit elit occaecat ullamco veniam. Ullamco sit laboris laborum ex veniam tempor ut. Aute excepteur esse consectetur aliqua sint irure consequat elit proident et occaecat ullamco. Duis laborum dolore fugiat dolore id eu laboris et. Velit pariatur quis qui adipisicing eiusmod sit velit esse consequat aliqua et anim culpa ex. Ea aliqua reprehenderit consectetur sunt magna veniam consectetur nostrud.
+            <br> 
+            <v-divider color="info"></v-divider>
+            Voluptate ea dolore deserunt nulla consectetur aliqua adipisicing eu labore labore Lorem in in ea. Ullamco incididunt qui elit enim labore consectetur reprehenderit veniam est eu amet ad et nulla. Anim pariatur consectetur veniam veniam reprehenderit voluptate incididunt id.
+            </p>
+        </div>
+        <div class="my-card col">
+            <p>Deserunt aliquip exercitation laborum magna sit enim ipsum nostrud. Ipsum irure elit incididunt ea occaecat commodo velit. Amet excepteur minim elit esse et reprehenderit. Veniam eiusmod ullamco occaecat amet incididunt sit voluptate elit.
+            </p>    
+        </div>
+        </v-card>
+
     </NuxtLayout>
 </template>
 
